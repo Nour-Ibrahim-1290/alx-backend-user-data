a7d036579c941706cca71a53b357f34ad15a1658
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-05-18 12:23:20 +0300

    PD-#0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(
