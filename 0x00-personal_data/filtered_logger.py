39a881654f7abef752cc2bb99a66dfbd3114dfc8
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-05-18 12:28:33 +0300

    PD-#0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,10 +2,14 @@
 """Personal Data"""
 
 import re
+from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """returns the log message obfuscated"""
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str
+        ) -> str:
+    """Returns the log message obfuscated
+    """
     for field in fields:
         message = re.sub(
             field + "=.*?" + separator,
