e0bed42dd8e7f95f67d71b076bef305e620253b1
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-05-18 13:14:19 +0300

    PD-#3

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Personal Data"""
 
-import logging
 import re
+import logging
+import mysql.connector
 from typing import List
 
 
@@ -36,6 +37,22 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Returns a connector to the database."""
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    db = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=db_name
+    )
+    return db
 
 
 class RedactingFormatter(logging.Formatter):
