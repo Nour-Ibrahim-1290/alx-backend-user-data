f903183646879a0c1ce0b93bcbd2f53bb09ee1e8
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-05-23 04:25:31 +0300

    basic-authentication-#4

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+from typing import List, TypeVar
+from flask import request
+
+
+User = TypeVar('User')
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method to require authentication """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ Method to get authorization header """
+        return None
+
+    def current_user(self, request=None) -> User:
+        """ Method to get current user """
+        return None
