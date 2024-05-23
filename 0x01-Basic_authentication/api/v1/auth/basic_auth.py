d68424378980e747291b26242bd7ea256dc2bbd2
Nour M. Ibrahim <nouribrahim1290@gmail.com>
2024-05-23 05:29:27 +0300

    basic-authentication-#8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Module of Basic Authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -18,3 +19,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """ Method to decode the Base64 Authorization """
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
+            return None
+        try:
+            message_bytes = base64.b64decode(base64_authorization_header)
+            message = message_bytes.decode('utf-8')
+            return message
+        except base64.binascii.Error:
+            return None
